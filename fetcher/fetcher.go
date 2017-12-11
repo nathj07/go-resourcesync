566c93cb0aaacff7d1d8e588636a0b97ff52e88c
Nathan Davies <ndavies@turnitin.com>
2017-12-11 15:57:23 +0000

    define the fetcher interface
    Next up is the basic implementation

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -5,3 +5,14 @@ package fetcher
 type Fetcher interface {
 	Fetch(source, dest string) error
 }
+
+// BasicRSFetcher is a simple implementation of the Fetcher interface. It is safe to use
+// but limited in capability. No timeouts or extra headers are defined. The general recommendation
+// is that the user of this client write their own implementation of the Fetcher interface.
+type BasicRSFetcher struct{}
+
+// Fetch retrieves the resource from source and writes it to dest. It is the callers responsibility
+// to clear up any local files when they are finished with.
+func (brf *BasicRSFetcher) Fetch(source, dest string) error {
+
+}
