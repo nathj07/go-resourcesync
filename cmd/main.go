a114e2bacd7576a3e264ef8288a7dccb114b0b00
Nathan Davies <ndavies@turnitin.com>
2017-12-12 21:20:03 +0000

    initial tool and basic parsing
    This includes parsing the ResourceList, there are structures for ResourceListIndex and simple approach to determining type.
    
    The tool wil parse a resourceList and print some details out. This nned to be imnproved and I need to make Parse() more general purpose.

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 
-// TODO
-// flags:
-//		target
-//		where to write data
-//		should we follow or get top level only
-//		verbose mode
+	"github.com/nathj07/go-resourcesync/resourcesync"
+)
 
+var (
+	target      = flag.String("target", "", "--target=http:/example.com/resourcesync.xml")
+	followIndex = flag.Bool("follow-index", false, "--follow-index means the tool will follow the links in a sitemaps index. Only one recursion is made")
+	verbose     = flag.Bool("verbose", false, "--verbose, if set will print all the links discovered")
+)
+
+// TODO switch fmt.Print to log.Info
 func main() {
-	fmt.Println("TO BE IMPLEMENTED")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage of %s:
+%s is a tool within the go-resourcesync client that allows you to visit a resourcesync endpoint and evaluate the response.
+Flags:
+`, os.Args[0], os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	u, err := url.Parse(*target)
+	if err != nil || !u.IsAbs() {
+		fmt.Println("This tool expects a --target flag to be passed in with a valid, absolute URL.")
+		os.Exit(1)
+	}
+
+	resources, err := checkResourceSync(*target)
+	if err != nil {
+		fmt.Printf("Error encountered checking resourcesync: %v\n", err)
+		os.Exit(2)
+	}
+
+	if !*followIndex {
+		fmt.Println("ResourceSync Data:")
+		printLinks(resources.URLSet)
+		os.Exit(0)
+	}
+}
+
+func checkResourceSync(addr string) (*resourcesync.ResourceList, error) {
+	// fetch it
+	b, err := retrieve(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	// send it to parse
+	rs := &resourcesync.ResourceSync{}
+	return rs.Parse(b)
+}
+
+func retrieve(addr string) ([]byte, error) {
+	res, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	b, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Returned Content Type: %s\n", res.Header.Get("Content-Type"))
+
+	return b, nil
+}
+
+func printLinks(resources []resourcesync.ResourceURL) {
+	if *verbose {
+		for _, r := range resources {
+			fmt.Printf("Loc: %+v\nLastMod: %+v\n\n", r.Loc, r.LastMod)
+		}
+	}
+	fmt.Printf("Total links found: %d\n", len(resources))
 }
--- resourcesync/resourcesync.go
+++ resourcesync/resourcesync.go
@@ -1,6 +1,24 @@
 package resourcesync
 
-import "github.com/nathj07/go-resourcesync/fetcher"
+import (
+	"bytes"
+	"encoding/xml"
+	"errors"
+
+	"github.com/nathj07/go-resourcesync/fetcher"
+)
+
+const (
+	// Unknown indicates we cannot determine the type of feed we are processing
+	Unknown = iota
+	// List indicates a ResourceList
+	List
+	// Index indicates a ResourceListIndex
+	Index
+)
+
+// ErrUnsupportedFeedType is used when the feed type is not one of the supported set
+var ErrUnsupportedFeedType = errors.New("unsupported feed type supplied")
 
 // ResourceSync is the top level structure needed to interact with ResourceSync endpoints
 type ResourceSync struct {
@@ -14,10 +32,93 @@ func New(f fetcher.RSFetcher) *ResourceSync {
 	}
 }
 
+// Parse handles the unmarshaling of the feed data.
+// TODO: MAKE THIS MORE GENERAL TO SERVE INDEX AND LIST OR HAVE DIFFERENT FUNCTIONS.
+// THINK ABOUT NICEST API FOR USER OF LIBRARY
+func (rs *ResourceSync) Parse(feed []byte) (*ResourceList, error) {
+	feedType := rs.determineType(feed)
+	switch feedType {
+	case Index:
+		return nil, ErrUnsupportedFeedType
+	case List:
+		rl := &ResourceList{}
+		if err := xml.Unmarshal(feed, rl); err != nil {
+			return nil, err
+		}
+		return rl, nil
+	default:
+		return nil, ErrUnsupportedFeedType
+	}
+
+}
+
+func (rs *ResourceSync) determineType(data []byte) int {
+	if bytes.Contains(data, []byte("<sitemapindex")) {
+		return Index
+	}
+	if bytes.Contains(data, []byte("<urlset")) {
+		return List
+	}
+	return Unknown
+}
+
 // TODO:
 // Function to retrieve data
 // Function to parse data - this should be exported and take a []byte to parse returning an exported struct
+// Consider not returning the raw dta structure but a simplified parsed data structure
 // Function to do it all in one go?
 // I think that for now the client will be very simple and it will be up to the caller to
 // determine how far to follow the chain.
 // In the future this could be changed with a slight variation/addition to the public API
+
+//
+// Data Structures
+//
+
+// ResourceListIndex defines the structure of a Resource List Index data set
+type ResourceListIndex struct {
+	XMLName     xml.Name   `xml:"sitemapindex"`
+	RSLink      []RSLN     `xml:"ln"`
+	RSMD        RSMD       `xml:"md"`
+	ResourceSet []IndexDef `xml:"sitemap"`
+}
+
+// ResourceList hods the data from a resource list
+type ResourceList struct {
+	XMLName xml.Name      `xml:"urlset"`
+	RSLink  []RSLN        `xml:"ln"`
+	RSMD    RSMD          `xml:"md"`
+	URLSet  []ResourceURL `xml:"url"`
+}
+
+// ResourceURL holds the data retrieved from the url tag set within a standard sitemap.xml
+type ResourceURL struct {
+	Loc        string `xml:"loc"`        // mandatory
+	LastMod    string `xml:"lastmod"`    // optional
+	ChangeFreq string `xml:"changefreq"` // optional
+	RSMD       RSMD   `xml:"md"`         // optional
+}
+
+// IndexDef holds those items defined as making up the resource list index data set
+type IndexDef struct {
+	Loc     string `xml:"loc"`     // mandatory
+	LastMod string `xml:"lastmod"` // optional
+	RSMD    RSMD   `xml:"rs:md"`   // optional
+}
+
+// RSLN is the namespaced ln values defined in the resourcesync protocol
+type RSLN struct {
+	Rel  string `xml:"rel"`
+	Href string `xml:"href"`
+}
+
+// RSMD is the namespaced md values defined in the resourcesync protocol
+type RSMD struct {
+	Capability string `xml:"capability,attr"`
+	At         string `xml:"at,attr"`
+	Completed  string `xml:"completed,attr"`
+	// the next three are used on ResourceList only
+	Hash   string `xml:"hash"`
+	Length string `xml:"length"`
+	Type   string `xml:"type"`
+}
