01c1af7974bd6bdf5eb38dd4881067f8de231b14
Nathan Davies <ndavies@turnitin.com>
2017-12-28 20:11:19 +0000

    updating the doc.go
    This puts a top level doc.go in place and makes the resourcesync doc.go more focussed.

--- resourcesync/doc.go
+++ resourcesync/doc.go
@@ -1,18 +1,5 @@
-// package resourcesync is a simple go library for intertacting with resourcesync data.
-// The resourcesync protocol is based on the pre-existing sitempa protocol and details can be found:
-// http://www.openarchives.org/rs/toc
-//
-// The motivation for this protocol is easing the discovery of academic research content and providing a
-// meachanism for updating those following the feed.
-//
-// With regards to this small library the miotivation is simply making is easy to consume the resourcesync data
-// within a Go app.
-//
-// The library makes no decisions about waht to do with content, it does not automatically follow any links within
-// the feed. Those decisions are left up to the caller.
-//
-// While the library does provide a simnple Fetcher implementation this is by no measn a production ready HTTP Fetcher.
-// It is expected that the user of the library will implement their own fetcher either injecting it into the
-// ResourceSync object or by fetching the data upfront first and simply calling the Parse function.
-
+// package resourcesync holds the main data structures and methods for reading a ResourceSync feed.
+// The main public functions allow you to either send in a ResourceSync feed URL, Process, or send in
+// []byte from a ResourceSync feed, Parse. In both cases you get a ResourceData object back with the
+// parsed data available for further inspection and use.
 package resourcesync
