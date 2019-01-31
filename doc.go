d86eae5f3cf19b6edc0c1ef6ab5bea56d36adb88
Nathan Davies <ndavies@turnitin.com>
2019-01-31 10:15:20 -0600

    fix typos in doc.go

--- doc.go
+++ doc.go
@@ -11,7 +11,7 @@
 // The library makes no decisions about what to do with content, it does not automatically follow any links within
 // the feed. Those decisions are left up to the caller.
 //
-// While the library does provide a simple Fetcher implementation this is by no measn a production ready HTTP Fetcher.
+// While the library does provide a simple Fetcher implementation this is by no means a production ready HTTP Fetcher.
 // It is expected that the user of the library will implement their own fetcher either injecting it into the
 // ResourceSync object or by fetching the data upfront first and simply calling the Parse function.
 
